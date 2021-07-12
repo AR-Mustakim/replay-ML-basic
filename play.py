c3bc00c1053c79376430dfec303ba7ebb727e118
hungryphobic <mustakim9419@gmail.com>
2021-07-13 06:28:57 +0900

    reupdate

---
+++ play.py
@@ -0,0 +1,33 @@
+"""Q1
+arr = [5,3,2,1,7]
+
+num=0
+for i in arr:
+    num = num + i
+print(num)
+     
+ """
+
+
+""" Q2
+arr = [2,5,8,7,13,17]
+#num=7
+# ans =3
+
+arr2=[]
+for i in arr:
+    if not i == 7 and i < 10:
+        arr2.append(i)
+
+print(len(arr2))
+"""
+
+def study():
+    ...
+
+def Alive():
+    life = 1
+    return True
+
+while Alive():
+    study()
